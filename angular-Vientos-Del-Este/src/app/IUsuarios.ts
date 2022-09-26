1bf2d0505a32e2fbcc12fb93ebf423a2735379f2
Franco De Stefano <FRANCO.DESTEFANO@correo.ucu.edu.uy>
2022-09-26 10:53:57 -0300

    AgregoListaDeHeroes

--- angular-Vientos-Del-Este/src/app/IUsuarios.ts
+++ angular-Vientos-Del-Este/src/app/IUsuarios.ts
@@ -1,4 +1,4 @@
-export enum IUsuarios{
+export enum IUSUARIOS{
     Administradores,
     Operarios,
     Auditores
---
+++ angular-Vientos-Del-Este/src/app/ListaDeUsuarios.ts
@@ -0,0 +1,6 @@
+import { IUSUARIOS } from './IUsuarios';
+import { Usuarios } from './Usuarios';
+
+export const USUARIOS: Usuarios[] = [
+  {    Id : 0,  Clase : IUSUARIOS.Administradores, Mail : 'Dr. Nice', Contraseña : "Sopa" }
+];
