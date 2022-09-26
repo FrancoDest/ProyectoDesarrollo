fe0b029adb13c7f3c747ac9ca6f63b82a1a7748d
FausUCU <88690038+FausUCU@users.noreply.github.com>
2022-09-26 10:51:16 -0300

    UsuarioAdmin

--- angular-Vientos-Del-Este/src/app/app.module.ts
+++ angular-Vientos-Del-Este/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { ModuloUsuarioComponent } from './modulo-usuario/modulo-usuario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    AdminLoginComponent,
+    ModuloUsuarioComponent
   ],
   imports: [
     BrowserModule,
