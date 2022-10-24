7dbd791d2b110e02a61a59db845a5ff5b275e22a
Franco De Stefano <FRANCO.DESTEFANO@correo.ucu.edu.uy>
2022-10-24 17:43:28 -0300

    Dejo el armado con piezas funcional

---
+++ angular-Vientos-Del-Este/src/app/Almacenamiento/ListaDePartes.ts
@@ -0,0 +1,10 @@
+import { IPartes } from '../IPartes';
+import { Partes } from '../Partes';
+
+
+export const PARTES: Partes[] = [
+  { Categoria : IPartes.A,Foto: "assets/aspaMetal.png",Altura : 33,ResistenciaEolica : 8484,Material : "Metal" },
+  {   Categoria :IPartes.B,Foto: "assets/BaseMadera.png",Altura : 48,ResistenciaEolica : 7474,Material : "Hierro" },
+  {   Categoria : IPartes.C,Foto: "assets/cuerpoHierro.png",Altura : 55,ResistenciaEolica : 666,Material : "MaderaLOL" },
+  {   Categoria :IPartes.B,Foto: "assets/BaseMadera.png",Altura : 49,ResistenciaEolica : 7474,Material : "Awa" },
+];
--- angular-Vientos-Del-Este/src/app/ListaDePartes.ts
+++
@@ -1,9 +0,0 @@
-import { IPartes } from './IPartes';
-import { Partes } from './Partes';
-
-
-export const PARTES: Partes[] = [
-  { Categoria : IPartes.A,Foto: "assets/aspaMetal.png",Altura : 33,ResistenciaEolica : 8484,Material : "Metal",  },
-  {   Categoria :IPartes.B,Foto: "assets/BaseMadera.png",Altura : 48,ResistenciaEolica : 7474,Material : "Hierro",    },
-  {   Categoria : IPartes.C,Foto: "assets/cuerpoHierro.png",Altura : 55,ResistenciaEolica : 666,Material : "MaderaLOL",    }
-];
