927ace590b632c9d299e612c5a296b607431ab9e
FausUCU <88690038+FausUCU@users.noreply.github.com>
2022-11-23 09:21:44 -0300

    loginConJWC

---
+++ angular-Vientos-Del-Este/src/app/Servicios/auth-service.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable,Component } from '@angular/core';
+import{interval,take,Observable,shareReplay, BehaviorSubject} from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  
+
+  constructor(private http:HttpClient) { }
+  login(username:String, password:String){
+    return this.http.post<any>('/users/login',{user:username,pass:password});
+
+  }
+}
